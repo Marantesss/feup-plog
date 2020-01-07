8b50dea8eac76a76d95bdf343bf874974b1bdb68
Marantesss <up201706917@fe.up.pt>
2020-01-07 10:38:37 +0000

    Almost finished test2 2019 last exercise

--- teste_2/teste_2_2019.pl
+++ teste_2/teste_2_2019.pl
@@ -64,7 +64,7 @@ constroi_bins(I, [V | Vs], [LBin | LBins]):-
 % EX 6
 /*
 Variaveis de decisão:
-     - lista em que o elemento de indice i indica em que comportimento o objeto correspondente deve ficar.
+     - lista em que o elemento de indice i indica em que compartimento o objeto correspondente deve ficar.
 Domínios:
      - Entre 1 e nº de compartimentos
 
@@ -77,11 +77,72 @@ Vars = [3, 1, 3, 4, 1, 4], significa que:
      - compartimento 4 vai conter o objeto de indice 4 e 6.
 
 Para resolver o problema é necessário restringir o volume de cada compartimento,
-pelo que preciso de obter a soma dos volumes d
-
+pelo que é necesário obter a soma dos volumes dos objetos cujo valor das variaveis
+em Vars sejam iguais, e restringir que essa soma não é maior que o volume máximo
+desse comportimento.
+Depois para aplicar a restrição do peso de cada compartimento ser inferior ao que está
+imediatamente abaixo, é necessário obter o peso de cada compartimento numa lista de
+sequência e restringir que esse peso não pode ser inferior ao daquele que estiver
+N casa antes, onde N é o número de compartimentos por linha.
 */
 
 % EX 7
 /*
+prat(+Prateleiras, +Objetos, -Vars)
 
+prat([[30, 6], [75, 15]], [176-40, 396-24, 474-35, 250-8, 149-5, 479-5], Vars).
+Vars = [3, 1, 3, 4, 1, 4]
+
+Matrix = [
+    [0, 1, 0, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0],
+    [1, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 1]
+]
 */
+prat(Prateleiras, Objetos, Vars):-
+    % ---- variables
+    % obter numero de prateleiras por linha e coluna
+    length(Prateleiras, NumeroLinhas),
+    nth1(1, Prateleiras, Linha),
+    length(Linha, NumeroColunas),
+    % obter numero total de prateleiras
+    NumeroPrateleiras is NumeroLinhas * NumeroColunas,
+    % construir matrix
+    length(Matrix, NumeroPrateleiras),
+    maplist(same_length(Objetos), Matrix),
+
+    % separar os objetos em duas listas (peso e dimensao)
+    keys_and_values(Objetos, PesoObjetos, EspacoObjetos),
+    
+    % ---- domain
+    % para cada prateleira
+    maplist(shelf_domain, Matrix),
+    % para cada objeto
+    transpose(Matrix, TMatrix),
+    maplist(object_domain, TMatrix),
+    
+    % ---- constraints
+    % o total de espaco ocupado nao pode ser maior que a capacidade da prateleira
+    append(Prateleiras, FlatPrateleiras),
+    maplist(shelf_constraint(EspacoObjetos), FlatPrateleiras, Matrix),
+
+    % ---- labeling
+    append(Matrix, FlatMatrix),
+    labeling([], FlatMatrix),
+	findall(ShelfIndex, (nth1(_ObjectIndex, TMatrix, Line), nth1(ShelfIndex, Line, 1)), Vars).
+    
+
+% para cada prateleira
+shelf_domain(Line):-
+    domain(Line, 0, 1).
+
+% para cada objeto
+object_domain(Line):-
+    domain(Line, 0, 1),
+    % cada objeto tem apenas uma prateleira associada
+    count(1, Line, #=, 1).
+
+shelf_constraint(EspacoObjetos, DimensaoPrateleira, ObjetosPrateleira):-
+    scalar_product(EspacoObjetos, ObjetosPrateleira, #=<, DimensaoPrateleira).
+    
